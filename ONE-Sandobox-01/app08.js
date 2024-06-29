288f5832cee7a595f9b71af334cd1dc969c48f59
Alicia_Lopez <aliciablopez@vivaldi.net>
2024-06-29 19:22:12 -0300

    App08

--- ONE-Sandobox-01/app08.js
+++ ONE-Sandobox-01/app08.js
@@ -1,17 +1,21 @@
 javascript
+// control del flujo: IF... THEN, 
 alert('Bienvenido al juego del número secreto');
 
-let numeroUsuario= prompt('Elige un número entre 1 y 10');
+let numeroUsuario= 0
 let numeroSecreto = 2;
 
-console.log(numeroUsuario == numeroSecreto);
+while (numeroUsuario != numeroSecreto){ // != evalua desigualdad (no iguall)
+  numeroUsuario = prompt('Elige un número entre 1 y 10');
+  console.log(numeroUsuario == numeroSecreto); // evalua igualdad
 
-if (numeroUsuario == numeroSecreto) {
-  alert('Acertaste, el número es': ${numeroUsuario});
-} else { // condicionales anidados
-    if (numeroUsuario > numeroSecreto) { //la condición es True
-      alert('El número secreto era ' + numeroSecreto + ', pero elegiste ' + numeroUsuario) +'. El número es menor'; // concatenar cadena y variable: ´str´ + variable
-    }else { // la condición es False            
-      alert('El número secreto era ' + numeroSecreto + ', pero elegiste ' + numeroUsuario) + `. El número es mayor` ; // concatenar cadena y variable: ´str´ + variable
-    }     
-}  
+  if (numeroUsuario == numeroSecreto) { // condición para True
+    alert('Acertaste, el número es' ${numeroUsuario});
+  } else { // Condición para False 
+      if (numeroUsuario > numeroSecreto) { //Condicional anidado, condición para True
+        alert('El número secreto era ' + numeroSecreto + ', pero elegiste ' + numeroUsuario) +'. El número es menor'; // concatenar cadena y variable: ´str´ + variable
+      }else { // condición para False            
+        alert('El número secreto era ' + numeroSecreto + ', pero elegiste ' + numeroUsuario) + `. El número es mayor` ; // concatenar cadena y variable: ´str´ + variable
+      }     
+  }  
+}
